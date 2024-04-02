caf8edb7a5246c85cf1cca7b46cb1bff9cc56828
Almeida250 <162496258+Almeida250@users.noreply.github.com>
2024-04-02 23:09:46 +0000

    agora dá pra escrever o nome do aluno como professor e colocar as notas desse aluno

--- diarioDeNotas.java
+++ diarioDeNotas.java
@@ -10,6 +10,21 @@ public class diarioDeNotas{
                 System.out.println("Digite o seu nome"); 
                 String nome = sc.nextLine();
                 System.out.println("Ola Professor " + nome + ".Seja bem vindo ");
+
+                System.out.println("Para registrar as notas avaliativas, insira o nome do aluno:");
+                String nomeAluno = sc.nextLine();
+
+
+                System.out.println("Coloque a nota da A1 do aluno " + nomeAluno + ":");
+                int nota1 = sc.nextInt();
+                System.out.println("Coloque a nota da A2 do aluno " + nomeAluno + ":");
+                int nota2 = sc.nextInt();
+                System.out.println("Coloque a nota da A3 do aluno " + nomeAluno + ":");
+                int nota3 = sc.nextInt();
+
+                int notaFinal = nota1 + nota2 + nota3;
+                System.out.println("O resultado final é:" + notaFinal);
+
             }else if(usuario.equalsIgnoreCase("Aluno")) {
                 System.out.println("Digite o seu nome"); 
                 String nome = sc.nextLine();
